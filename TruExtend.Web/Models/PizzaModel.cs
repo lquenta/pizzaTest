b1bd7b86ff507f75db3601b46a7994acfa17eddd
Leo Quenta <lquenta@gmail.com>
2019-07-02 18:07:00 -0400

    Initial Architecture and project creation

---
+++ TruExtend.Web/Models/PizzaModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TruExtend.Web.Models
+{
+    public class Pizzas
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+
+    public class Topping
+    {
+        
+    }
+    
+}
---
+++ TruExtend.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TruExtend.Web.Startup))]
+namespace TruExtend.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
